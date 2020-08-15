73e3bd253649259d91e127a36c46dd7dae1503a0
imranmaj <49664304+imranmaj@users.noreply.github.com>
2020-08-15 14:07:31 -0700

    Receive and send messages

--- bot.py
+++ bot.py
@@ -1,16 +1,20 @@
 import os
 
-import discord
 from discord.ext import commands
-from dotenv import load_dotenv
+import requests
+# from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+PREFIX = "!"
+COMMAND_NAME = "run"
 
-bot = commands.Bot(command_prefix='')
+bot = commands.Bot(command_prefix=PREFIX)
 
-@bot.command(name='run')
+@bot.command(name=COMMAND_NAME)
 async def run(ctx):
-    await ctx.send('received')
+    content = ctx.message.content.lstrip(PREFIX + COMMAND_NAME)
+    # requests.post("127.0.0.1:8000", data=content)
+    await ctx.send(content)
 
 bot.run(TOKEN)
