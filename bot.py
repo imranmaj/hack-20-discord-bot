6b4deb3f4a664fc7dfd17110520bb039b851f777
adeodhar <adeodhar@cs.washington.edu>
2020-08-15 15:25:21 -0700

    Able to parse out language and code

--- bot.py
+++ bot.py
@@ -8,13 +8,24 @@ import requests
 TOKEN = os.getenv('DISCORD_TOKEN')
 PREFIX = "!"
 COMMAND_NAME = "run"
-
+BOUNDS = "```"
 bot = commands.Bot(command_prefix=PREFIX)
 
 @bot.command(name=COMMAND_NAME)
 async def run(ctx):
     content = ctx.message.content.lstrip(PREFIX + COMMAND_NAME)
-    r = requests.post("http://localhost:8000", json={"content": content})
+    content = content.strip()
+    content = content.strip(BOUNDS)
+
+    language = ""
+    for i in content:
+        if i == "\n":
+            break
+        language += i
+    content = content[len(language)+1::]
+    
+    r = requests.post("http://localhost:8000", json={"content": content, "language": language})
+    
     await ctx.send(r.text)
 
 bot.run(TOKEN)
--- server.py
+++ server.py
@@ -7,10 +7,15 @@ app = Flask(__name__)
 @app.route("/", methods=["POST"])
 def run():
     content = request.json["content"]
+    lang = request.json["language"]
+
+    '''
     output = StringIO()
     sys.stdout = output
     eval(content)
     return output.getvalue()
+    '''
 
+    return f"Language = {lang} and Code = \n{content}"
 if __name__ == "__main__":
     app.run("0.0.0.0", 8000)
