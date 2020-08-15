b23b794ee09774dc56b3479cda590a13fc7886cc
imranmaj <49664304+imranmaj@users.noreply.github.com>
2020-08-15 14:27:24 -0700

    Redirect stdout to request response

--- server.py
+++ server.py
@@ -1,11 +1,16 @@
 from flask import Flask, request, abort
+import sys
+from io import StringIO
 
 app = Flask(__name__)
 
 @app.route("/", methods=["POST"])
 def run():
     content = request.json["content"]
-    return str(eval(content))
+    output = StringIO()
+    sys.stdout = output
+    eval(content)
+    return output.getvalue()
 
 if __name__ == "__main__":
     app.run("0.0.0.0", 8000)
