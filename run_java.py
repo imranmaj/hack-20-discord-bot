b7bee54328fdcf2da1ce0fca6b61a5e422a526c5
William Siauw <36526721+Volcore001@users.noreply.github.com>
2020-08-15 21:58:13 -0700

    Write to .java

---
+++ run_java.py
@@ -0,0 +1,15 @@
+import os, traceback, sys
+
+from timeout import timeout
+from io import StringIO
+
+@timeout(15)
+def run_python(content):
+    file = open("JavaSkeleton.java","r")
+
+    for x in range(0,3):
+        file.readlines()
+    file = open("JavaSkeleton.java", "a")
+    file.write(content)
+    # run the java somehow?!?!?
+    file.close()
