99c444758580647ee94dff860e83f1c2dffd4237
imranmaj <49664304+imranmaj@users.noreply.github.com>
2020-08-15 17:25:27 -0700

    Deploy to google cloud

--- bot.py
+++ bot.py
@@ -8,6 +8,10 @@ TOKEN = os.getenv("DISCORD_TOKEN")
 PREFIX = "!"
 COMMAND_NAME = "run"
 BOUNDS = "```"
+LANGUAGE_ENDPOINTS = {
+    # "python": "",
+    "java": ""
+}
 
 bot = commands.Bot(command_prefix=PREFIX)
 
@@ -18,7 +22,7 @@ async def run(ctx):
     content = ctx.message.content.lstrip(PREFIX + COMMAND_NAME)
     # remove leading and trailing whitespace
     content = content.strip()
-    # remove ````
+    # remove ```
     content = content.strip(BOUNDS)
 
     # extract language
@@ -29,13 +33,19 @@ async def run(ctx):
         language += i
     content = content[len(language) + 1 :]
 
+    # check if endpoint exists
+    if language.lower() not in LANGUAGE_ENDPOINTS:
+        await ctx.send("error: invalid language")
+        return
+
     # send to server
-    r = requests.post(
-        "http://localhost:8000", json={"content": content, "language": language}
-    )
+    # r = requests.post(
+    #     LANGUAGE_ENDPOINTS[language], json={"content": content}
+    # )
 
     # send eval output to discord
-    await ctx.send(r.text)
+    # await ctx.send(r.text)
+    await ctx.send(content)
 
 
 if __name__ == "__main__":
